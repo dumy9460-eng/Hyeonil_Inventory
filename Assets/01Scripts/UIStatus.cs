0ac1879c0765803f86a79df4f961678885c3c923
Dumy8 <dumy9460@gmail.com>
2025-11-27 19:53:25 +0900

    feat: UIStatus.cs

---
+++ Assets/01Scripts/UIStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIStatus : MonoBehaviour
+{
+    [Header("Stat Values")]
+    [SerializeField] private TextMeshProUGUI attackValueText;
+    [SerializeField] private TextMeshProUGUI defenseValueText;
+    [SerializeField] private TextMeshProUGUI hpValueText;
+    [SerializeField] private TextMeshProUGUI criticalValueText;
+
+    [Header("Button")]
+    [SerializeField] private Button backButton;
+
+    private void Start()
+    {
+        backButton.onClick.AddListener(GoBack);
+    }
+
+    // 캐릭터 정보 업데이트
+    public void UpdateCharacterInfo(Character character)
+    {
+        attackValueText.text = character.TotalAttack.ToString();
+        defenseValueText.text = character.TotalDefense.ToString();
+        hpValueText.text = character.TotalHP.ToString();
+        criticalValueText.text = character.TotalCritical.ToString();
+    }
+
+    private void GoBack()
+    {
+        gameObject.SetActive(false);
+        UIManager.Instance.MainMenu.gameObject.SetActive(true);
+        UIManager.Instance.MainMenu.UpdateCharacterInfo(GameManager.Instance.Player);
+    }
+}
