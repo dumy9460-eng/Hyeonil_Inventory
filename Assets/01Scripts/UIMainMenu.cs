9423713f3ce8bb7517f77b138748f6d93a2c77cb
Dumy8 <dumy9460@gmail.com>
2025-11-27 19:54:12 +0900

    feat: UIMainMenu.cs

---
+++ Assets/01Scripts/UIMainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [Header("Character Info")]
+    [SerializeField] private TextMeshProUGUI jobText;
+    [SerializeField] private TextMeshProUGUI characterNameText;
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI expText;
+    [SerializeField] private Image expBarFill;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private TextMeshProUGUI goldText;
+
+    [Header("Buttons")]
+    [SerializeField] private Button statusButton;
+    [SerializeField] private Button inventoryButton;
+
+    private void Start()
+    {
+        // 버튼 이벤트 연결
+        statusButton.onClick.AddListener(OpenStatus);
+        inventoryButton.onClick.AddListener(OpenInventory);
+    }
+
+    // 캐릭터 정보 업데이트
+    public void UpdateCharacterInfo(Character character)
+    {
+        jobText.text = character.Job;
+        characterNameText.text = character.Name;
+        levelText.text = $"LV {character.Level}";
+        expText.text = $"{character.Exp} / {character.MaxExp}";
+        expBarFill.fillAmount = (float)character.Exp / character.MaxExp;
+        descriptionText.text = character.Description;
+        goldText.text = character.Gold.ToString("N0"); // 천 단위 콤마
+    }
+
+    private void OpenStatus()
+    {
+        gameObject.SetActive(false);
+        UIManager.Instance.Status.gameObject.SetActive(true);
+        UIManager.Instance.Status.UpdateCharacterInfo(GameManager.Instance.Player);
+    }
+
+    private void OpenInventory()
+    {
+        gameObject.SetActive(false);
+        UIManager.Instance.Inventory.gameObject.SetActive(true);
+        UIManager.Instance.Inventory.UpdateInventory(GameManager.Instance.Player);
+    }
+}
